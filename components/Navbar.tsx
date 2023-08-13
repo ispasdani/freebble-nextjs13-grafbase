204608d0ce06e87035b5b106b44a724f13cac605
ispasdani <ispasdani14@gmail.com>
2023-08-13 22:41:37 +0200

    commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { NavLinks } from "@/constants/index";
 import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -24,16 +26,8 @@ const Navbar = async () => {
       <div className="flexCenter gap-4">
         {session?.user ? (
           <>
-            {session?.user?.image && (
-              <Image
-                src={session.user.image}
-                width={40}
-                height={40}
-                className="rounded-full"
-                alt={session.user.name}
-              />
-            )}
-            <Link href="create-project">Share Work</Link>
+            <ProfileMenu session={session} />
+            <Link href="/create-project">Share Work</Link>
           </>
         ) : (
           <AuthProviders />
