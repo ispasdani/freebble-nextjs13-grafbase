921db56c27939ea893fed459d3cd0bbbc7a5c08b
ispasdani <ispasdani14@gmail.com>
2023-08-20 22:36:05 +0200

    4:05 Categories page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,53 @@
-const Home = () => {
+import { ProjectInterface } from "@/common.types";
+import Categories from "@/components/Categories";
+import ProjectCard from "@/components/ProjectCard";
+import { fetchAllProjects } from "@/lib/actions";
+
+type ProjectSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+    };
+  };
+};
+
+const Home = async () => {
+  const data = (await fetchAllProjects()) as ProjectSearch;
+
+  const projectsToDisplay = data?.projectSearch?.edges || [];
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
+        Categories
+        <p className="no-result-text text-center">
+          No projects found, go create some first.
+        </p>
+      </section>
+    );
+  }
+
   return (
-    <div>
-      <section className="flex-start flex-col paddings mb-16"></section>
-    </div>
+    <section className="flex-start flex-col paddings mb-16">
+      <Categories />
+
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node?.id}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
+          />
+        ))}
+      </section>
+    </section>
   );
 };
 
